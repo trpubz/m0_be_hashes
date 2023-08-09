8fc2f38000923a2fc77d73e8741fcbd0c3516f39
Taylor Pubins <tpubz@icloud.com>
2023-08-09 11:35:08 -0700

    [hashes_1] complete

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,31 @@
-
-Activity:
-
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
 foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
 
-# Write code that prints all of the 'keys' of the foods variable 
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the foods variable 
+foods.each do | key, _ |
+  p key
+end
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+foods.each do | _, value |
+  p value
+end
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+properly_stored_foods = foods.to_a
+food, value = properly_stored_foods[1]
+p value
 
-# Write code that adds a food to the foods hash. 
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods[:"raspberries"] = 16900
+p foods
 
 
 #-------------------
@@ -36,17 +38,27 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
-
+email = {
+  sender_name: "Taylor P",
+  sender: "tpubz@icloud.com",
+  recipient: "tp@trpubz.dev",
+  subject: "Inquiry",
+  cc: nil,
+ }
 # Write code that prints your email hash to the terminal.
+p email
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+email.each do | key, _ |
+  p key
+end
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+email.each do | _, value |
+  p value
+end
 
 
 #-------------------
@@ -74,7 +86,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -88,12 +100,38 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+emails = [
+  {
+    sender_name: "Taylor P",
+    sender: "tpubz@icloud.com",
+    recipient: "tp@trpubz.dev",
+    subject: "Inquiry",
+    cc: nil,
+ },
+ {
+    sender_name: "Amanda P",
+    sender: "apubins@icloud.com",
+    recipient: "tp@trpubz.dev",
+    subject: "Hello",
+    cc: "nana@icloud.com",
+ },
+ {
+    sender_name: "Sebass P",
+    sender: "tp@trpubz.dev",
+    recipient: "bossman@corporate.com",
+    subject: "Please don't fire me but...",
+    cc: :ALL,
+ },
+]
+
+p emails
